1136377c11e805ef39cbc026f490812c0776f1df
erenyusufduran <eren.duran@colins.com>
2023-03-14 19:15:27 +0300

    socket setup

--- chat-app/src/app.js
+++ chat-app/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 require("./db/mongodb");
 const userRouter = require("./routers/user");
@@ -5,8 +6,11 @@ const roomRouter = require("./routers/room");
 
 const app = express();
 
+const publicDirectoryPath = path.join(__dirname, "../public");
+
 app.use(express.json());
-app.use("/users", userRouter);
-app.use("/rooms", roomRouter);
+app.use(express.static(publicDirectoryPath));
+app.use("/api/users", userRouter);
+app.use("/api/rooms", roomRouter);
 
 module.exports = app;
--- chat-app/src/server.js
+++ chat-app/src/server.js
@@ -1,4 +1,10 @@
+const http = require("http");
+const socketio = require("socket.io");
 const app = require("./app");
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log("Server is listening on port", PORT));
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => console.log("Server is listening on port", PORT));
