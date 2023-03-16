5f47bf0befd77ec6d80ee1b6197c29f28277d548
erenyusufduran <eren.duran@colins.com>
2023-03-16 10:09:25 +0300

    home page submit and actions, chat page

--- chat-app/public/js/home.js
+++ chat-app/public/js/home.js
@@ -3,3 +3,15 @@ const homeOnload = () => {
     window.location.replace("/login.html");
   }
 };
+
+const roomJoinForm = document.querySelector("#room-join-form");
+
+roomJoinForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const response = await axios.post("api/rooms", {
+    name: e.target[0].value,
+  });
+  console.log(response);
+  const room = response.data;
+  window.location.replace(`/chat.html?room=${room._id}`);
+});
