da415ef42baa08737ac5b895c9fd25b4c8866dca
erenyusufduran <eren.duran@colins.com>
2023-03-17 11:23:42 +0300

    scheduler update

--- moderator-app/src/scheduler/scheduler.js
+++ moderator-app/src/scheduler/scheduler.js
@@ -3,7 +3,7 @@ const { addToDb, initManagement } = require("../db/loaders/migrations");
 const { chooseSupporters, updateChoosedSupporter } = require("./jobs/chooseSupporters");
 
 // every week on mondays at 7:45 AM
-const cronMethod = cron.schedule("*/10 * * * * *", async () => {
+const cronMethod = cron.schedule("45 7 * * 1", async () => {
   await addToDb();
   const management = await initManagement();
   const sprintCount = management.sprintCount;
