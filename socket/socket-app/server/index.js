147b25acfd12edf7b108bc5b458f6f95a5af13c6
erenyusufduran <eren.duran@colins.com>
2023-03-19 16:47:19 +0300

    socket rooms

--- socket/socket-app/server/index.js
+++ socket/socket-app/server/index.js
@@ -16,8 +16,31 @@ const io = new Server(server, {
   },
 });
 
+const CHAT_BOT = "ChatBot";
+
+let chatRoom = "";
+let allUsers = [];
+
 io.on("connection", (socket) => {
   console.log(`User connected ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    const { username, room } = data;
+    socket.join(room);
+
+    let createdTime = Date.now();
+    socket.to(room).emit("receive_message", {
+      message: `Welcome ${username}`,
+      username: CHAT_BOT,
+      createdTime,
+    });
+
+    chatRoom = room;
+    allUsers.push({ id: socket.id, username, room });
+    const chatRoomUsers = allUsers.filter((user) => user.room === room);
+    socket.to(room).emit("chatroom_users", chatRoomUsers);
+    socket.emit("chatroom_users", chatRoomUsers);
+  });
 });
 
 server.listen(PORT, () => `Server is running on port ${PORT}`);
