3c39d3b0d3606b49054199c9cf542928e46933dd
erenyusufduran <eren.duran@colins.com>
2023-07-01 18:13:38 +0300

    todo model

---
+++ typescript-app/src/models/todo.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+interface ITodo {
+  title: string;
+  description: string;
+}
+
+interface TodoDoc extends mongoose.Document {
+  title: string;
+  description: string;
+}
+
+interface TodoModelInterface extends mongoose.Model<TodoDoc> {
+  build(attr: ITodo): TodoDoc;
+}
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+todoSchema.statics.build = (attr: ITodo) => {
+  return new Todo(attr);
+};
+
+const Todo = mongoose.model<TodoDoc, TodoModelInterface>("Todo", todoSchema);
+
+Todo.build({
+  title: "Eren",
+  description: "eren",
+});
+
+export { Todo };
