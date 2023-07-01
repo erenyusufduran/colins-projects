0a7c78fcf9b8145a3f812ae9fb4d1d29f8137f22
erenyusufduran <eren.duran@colins.com>
2023-07-01 18:27:27 +0300

    todo routes finished

--- typescript-app/src/routes/todo.ts
+++ typescript-app/src/routes/todo.ts
@@ -1,13 +1,23 @@
 import express, { Request, Response } from "express";
+import { Todo } from "../models";
 
 const router = express.Router();
 
-router.get("/api/todo", (req: Request, res: Response) => {
-  return res.send("the todo");
+router.get("/api/todos", async (req: Request, res: Response) => {
+  const todos = await Todo.find({});
+  return res.status(200).send(todos);
 });
 
-router.post("/api/todo", (req: Request, res: Response) => {
-  return res.send("new todo created");
+router.post("/api/todo", async (req: Request, res: Response) => {
+  const { title, description } = req.body;
+
+  try {
+    const todo = Todo.build({ title, description });
+    await todo.save();
+    return res.status(201).send(todo);
+  } catch (error) {
+    return res.status(500).send({ error: "Something went wrong!" });
+  }
 });
 
 export { router as todoRouter };
