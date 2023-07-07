975a2715b3e7bb0a69334276bc689a39ade99f3c
erenyusufduran <eren.duran@colins.com>
2023-07-07 19:43:34 +0300

    service interface and TodoService class

---
+++ typescript-app-v2/interfaces/service.interface.ts
@@ -0,0 +1,9 @@
+import { Filter, UpdateFilter } from "mongodb";
+
+export interface Service<T> {
+  find(query: Filter<T>): Promise<T[]>;
+  findOne(query: Filter<T>): Promise<T | null>;
+  create(params: T): Promise<T>;
+  update?(query: Filter<T>, updateObject: UpdateFilter<T>): Promise<T | null>;
+  delete?(query: Filter<T>): Promise<T | Boolean>;
+}
---
+++ typescript-app-v2/services/todo.service.ts
@@ -0,0 +1,26 @@
+import { Filter } from "mongodb";
+import { Todo, TodoWithId, Todos } from "../models/todo.model";
+import { Service } from "../interfaces/service.interface";
+
+export class TodoService implements Service<Todo> {
+  find = async (query?: Filter<TodoWithId>): Promise<TodoWithId[]> => {
+    const todos = await Todos.find({ query }).toArray();
+    return todos;
+  };
+
+  findOne = async (query: Filter<TodoWithId>): Promise<TodoWithId | null> => {
+    const todo = await Todos.findOne({ query });
+    if (todo) return todo;
+    return null;
+  };
+
+  findCompletedTodos = async () => {};
+
+  findNoncompletedTodos = async () => {};
+
+  create = async (_todo: Todo): Promise<TodoWithId> => {
+    const todo = await Todo.parseAsync(_todo);
+    const insertedTodo = await Todos.insertOne(todo);
+    return { _id: insertedTodo.insertedId, ...todo };
+  };
+}
