951c6d6226f39a3666e12c37b6132128765553b6
erenyusufduran <eren.duran@colins.com>
2023-07-12 15:14:05 +0300

    mongoloader with microframework

--- typescript-app-v2/loaders/mongo.loader.ts
+++ typescript-app-v2/loaders/mongo.loader.ts
@@ -1,10 +1,16 @@
+import { MicroframeworkLoader, MicroframeworkSettings } from "microframework";
 import { MongoClient } from "mongodb";
 
 const mongoClient = new MongoClient("mongodb://localhost:27017/todo");
 export const db = mongoClient.db();
-export const mongoLoader = async () => {
+export const mongoLoader: MicroframeworkLoader = async (settings: MicroframeworkSettings | undefined) => {
   try {
     await mongoClient.connect();
+    if (settings) {
+      settings.setData("mongo_client", mongoClient);
+      settings.setData("db", db);
+      settings.onShutdown(() => mongoClient.close());
+    }
   } catch (error) {
     throw new Error((error as Error).message);
   }
