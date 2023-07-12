1155a2c7d76853c39460aaac66f0f4301639cc9a
erenyusufduran <eren.duran@colins.com>
2023-07-12 15:18:18 +0300

    port

--- typescript-app-v2/loaders/express.loader.ts
+++ typescript-app-v2/loaders/express.loader.ts
@@ -2,14 +2,16 @@ import { createExpressServer } from "routing-controllers";
 import { TodoController } from "../api/controller/todo.controller";
 import { MicroframeworkLoader, MicroframeworkSettings } from "microframework";
 
-export const expressLoader : MicroframeworkLoader = async (settings: MicroframeworkSettings | undefined) => {
+const PORT = process.env.PORT || 3000;
+
+export const expressLoader: MicroframeworkLoader = async (settings: MicroframeworkSettings | undefined) => {
   const expressApp = createExpressServer({
     routePrefix: "/api",
     controllers: [TodoController],
     classTransformer: false,
   });
 
-  const server =  expressApp.listen(3000, () => console.log("server is running"));
+  const server = expressApp.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
   settings?.setData("express_app", server);
   settings?.setData("express_app", expressApp);
