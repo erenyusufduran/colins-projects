86f357384e0c82c588486706101933565171b498
erenyusufduran <eren.duran@colins.com>
2023-07-13 20:31:40 +0300

    trade types

--- typescript-app/src-diff/database/models/trades/trades.types.ts
+++ typescript-app/src-diff/database/models/trades/trades.types.ts
@@ -1,43 +1,49 @@
 import { Document, Model } from "mongoose";
 
 export enum TimeframeOptions {
-  M1,
-  M3,
-  M5,
-  M15,
-  M30,
-  M45,
-  H1,
-  H2,
-  H3,
-  H4,
-  H6,
-  H8,
-  H12,
-  D1,
-  D3,
-  W1,
-  MO1,
-  MO3,
+  M1 = "1 minute",
+  M3 = "3 minutes",
+  M5 = "5 minutes",
+  M15 = "15 minutes",
+  M30 = "30 minutes",
+  M45 = "45 minutes",
+  H1 = "1 hour",
+  H2 = "2 hours",
+  H3 = "3 hours",
+  H4 = "4 hours",
+  H6 = "6 hours",
+  H8 = "8 hours",
+  H12 = "12 hours",
+  D1 = "1 day",
+  D3 = "3 days",
+  W1 = "1 week",
+  MO1 = "1 month",
+  MO3 = "3 months",
 }
 
 export interface ITrade {
-  date: Date;
-  pair: string;
-  timeframe: TimeframeOptions;
+  date?: Date;
+  pair?: string;
+  timeframe?: {
+    type: String;
+    enum: TimeframeOptions;
+  };
   shortLong: boolean;
   entry: number;
-  tp: number;
-  sl: number;
-  size: number;
-  r: number;
-  profit: number;
-  wl: boolean;
-  link: string;
-  reasons: string;
-  results: string;
+  tp?: number;
+  sl?: number;
+  size?: number;
+  wl?: boolean;
+  link?: string;
+  reasons?: string;
+  results?: string;
 }
 
-export interface ITradeDocument extends ITrade, Document {}
+export interface ITradeDocument extends ITrade, Document {
+  r: number;
+  profit: number;
+}
 
-export interface ITradeModel extends Model<ITradeDocument> {}
+export interface ITradeModel extends Model<ITradeDocument> {
+  build: (trade: ITrade) => Promise<ITradeDocument>;
+}
