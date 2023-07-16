1248f6a7926f83534379805c1f3938097f8235be
erenyusufduran <eren.duran@colins.com>
2023-07-16 16:58:54 +0300

    todo controller

--- typescript-psql-typeorm/src-v2/api/controllers/TodoController.ts
+++ typescript-psql-typeorm/src-v2/api/controllers/TodoController.ts
@@ -1,16 +1,51 @@
-import { Controller, Get, Post, Body, Res, Param } from "routing-controllers";
-import { Response } from "express";
+import { Controller, Get, Post, Body, Res, Param, UseBefore, Delete } from "routing-controllers";
+import { Response, json } from "express";
 import { Todo } from "../../models/Todo";
 
 @Controller("/todos")
 export class TodoController {
   @Get()
-  async getAll(@Res() res: Response) {
+  async getAll(@Res() res: Response): Promise<Response<Todo[]>> {
     try {
       const todos = await Todo.find();
-      return res.status(200).send({ results: todos });
+      return res.status(200).send({ result: todos });
     } catch (error) {
-      return res.status(500).send((error as Error).message);
+      return res.status(500).send(error.message);
+    }
+  }
+
+  @Get("/:id")
+  async getWithId(@Param("id") id: string, @Res() res: Response): Promise<Response<Todo>> {
+    try {
+      const todo = await Todo.findOneBy({ id });
+      if (todo) return res.status(200).send({ result: todo });
+      return res.status(404).send({ result: `Couldn't find todo with ${id}` });
+    } catch (error) {
+      return res.status(400).send(error.message);
+    }
+  }
+
+  @Post("/")
+  @UseBefore(json())
+  async create(@Body() body: Todo, @Res() res: Response): Promise<any> {
+    try {
+      const todo = Todo.create(body);
+      await todo.save();
+      return res.status(201).send(todo);
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+
+  @Delete("/:id")
+  async delete(@Param("id") id: string, @Res() res: Response): Promise<any> {
+    try {
+      const todo = await Todo.findOneBy({ id });
+      if (!todo) throw new Error(`There is no todo with this id ${id}`);
+      todo.remove();
+      return res.send({ result: "Todo has been removed" });
+    } catch (error) {
+      return res.status(400).send(error.message);
     }
   }
 }
