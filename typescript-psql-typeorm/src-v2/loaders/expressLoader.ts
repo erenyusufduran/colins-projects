a1e19ec04848e30100fa9190957843aa79b4affa
erenyusufduran <eren.duran@colins.com>
2023-07-19 13:27:26 +0300

    auth middleware added to express loader

--- typescript-psql-typeorm/src-v2/loaders/expressLoader.ts
+++ typescript-psql-typeorm/src-v2/loaders/expressLoader.ts
@@ -1,5 +1,6 @@
 import { createExpressServer } from "routing-controllers";
 import { TodoController, UserController } from "../api/controllers";
+import { AuthMiddleware } from "src-v2/api/middlewares/AuthMiddleware";
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +8,7 @@ export const expressLoader = async () => {
   const expressApp = await createExpressServer({
     routePrefix: "/api",
     controllers: [UserController, TodoController],
+    middlewares: [AuthMiddleware],
     classTransformer: false,
   });
   expressApp.listen(PORT, () => console.log("Express server is running on port " + PORT));
