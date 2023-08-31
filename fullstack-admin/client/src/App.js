26705f9721797a1a08dd39ca1e042a32fbcb4f1a
erenyusufduran <eren.duran@colins.com>
2023-09-01 01:11:48 +0300

    building navbar with mui

--- fullstack-admin/client/src/App.js
+++ fullstack-admin/client/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { themeSettings } from "theme";
+
+import Layout from "scenes/layout"
+import Dashboard from "scenes/dashboard";
 
 const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div>App</div>
-  )
-}
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
